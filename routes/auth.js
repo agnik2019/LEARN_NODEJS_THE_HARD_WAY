fc839356fa0a89f335c3fe2d10305ec6706c495c
Agnik Saha <agniksaha2019@gmail.com>
2021-06-02 18:21:12 +0530

    response header

--- routes/auth.js
+++ routes/auth.js
@@ -21,9 +21,10 @@ router.post('/', async (req, res) => {
     if(!validPassword)
         return res.status(400).send('Invalid email or password');
    
-    const token = jwt.sign({_id : user._id}, config.get('jwtPrivateKey'));
+    const token = user.generateAuthToken();
     res.send(token );
 });
+//Information Expert Principle
 
 function validate(req) {
     const schema = {
