c645e43c19de70431329bd2a92c998b7784b0074
Agnik Saha <agniksaha2019@gmail.com>
2021-08-05 11:17:52 +0530

    object destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ const forecast = (latitude , longitude, callback) => {
 
     const url ='http://api.weatherstack.com/current?access_key=3a97ca219bae8fcff391e93a56ed6ecb&query='+latitude+','+longitude;//+'&units=f';
 
-    request({url:url, json: true}, (error, response)=> {
+    request({url, json: true}, (error, {body})=> {
         if(error){
             callback('unable to connect to weather service',undefined);
-        } else if(response.body.error){
+        } else if(body.error){
             callback('unable to find location', undefined);
         } else {
-            callback(undefined,response.body.current.weather_descriptions[0]+". It is currently "+response.body.current.temperature+" and Real feel is "+response.body.current.feelslike)
+            callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" and Real feel is "+body.current.feelslike)
         }
 
     })
